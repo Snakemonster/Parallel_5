c06d574fa775dfb9dfd4121f07572cf365e1a8e1
Makuta <nikita.o.platonov@gmail.com>
2022-08-19 09:25:27 +0300

    It just little refactor for XORCipher FS

--- XORCipher FS/XORCipher.fs
+++ XORCipher FS/XORCipher.fs
@@ -1,23 +1,24 @@
 ﻿namespace XORCipher_FS
 open System
+open System.Text
 
 type public XORCipher() =
     static member GetRandomKey(key: int, length:int) =
-        let mutable gamma = ""
+        let gamma = StringBuilder()
         let rnd = Random(key)
-        for i = 0 to length do gamma <- gamma + string(char(rnd.Next(35, 126)))
-        gamma
+        for i = 0 to length do gamma.Append(string(char(rnd.Next(35, 126)))) |> ignore
+        gamma.ToString()
     
     member private this.GetRepeatKey(s:string) (n:int) =
-        let mutable r = s
-        while r.Length < n do r <- r + r
-        r.Substring(0, n)
+        let r = StringBuilder(s)
+        while r.Length < n do r.Append(r)
+        r.ToString().Substring(0, n)
     
     member private this.Cipher(text: string) (secretKey: string) =
         let currentKey = this.GetRepeatKey secretKey text.Length
-        let mutable res = ""
-        for i = 0 to text.Length - 1 do res <- res + string(char(int(text[i]) ^^^ int(currentKey[i])))
-        res
+        let res = StringBuilder() 
+        for i = 0 to text.Length - 1 do res.Append(string(char(int(text[i]) ^^^ int(currentKey[i])))) |> ignore
+        res.ToString()
         
     member public this.Encrypt(plainText: string, password: string) = this.Cipher plainText password
         
